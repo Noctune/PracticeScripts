4100ffaae038a51ff4d4f786d53e2cef1bcb93e9
Nocturne <60099362+Noctune@users.noreply.github.com>
2025-11-13 11:13:44 +0800

    Refactor tkinter clock label and window positioning

--- tkinter_digital_clock.py
+++ tkinter_digital_clock.py
@@ -2,7 +2,6 @@
 import tkinter as tk
 from datetime import datetime
 
-
 def update():
     delay = 98
     now = datetime.now()
@@ -14,11 +13,17 @@ def update():
 
 if __name__ == '__main__':
     root = tk.Tk()
-    lable = tk.Label(root, text=datetime.strftime(datetime.now(), "%H:%M:%S"), foreground="red", background="#ff2121",
+    lable = tk.Label(root, foreground="red", background="#ff2121",
                      font=('Segoe Script', 16))
     lable.pack()
+    # 无边框
     root.overrideredirect(True)
-    root.geometry("+1860+0")  # 900 for 1080p
+    # 获取所需的窗口宽度
+    window_width = root.winfo_reqwidth()
+    screen_width = root.winfo_screenwidth()
+    # 计算居中的x位置
+    x = (screen_width - window_width) // 2 + 30
+    root.geometry(f"+{x}+0")
     root.wm_attributes('-topmost', 1)
     # root.attributes('-alpha', 0.8)
     root.attributes('-transparentcolor', "#ff2121")
